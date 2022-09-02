14334f6f351442b33c9f4a742a8c826db339c6e4
yanhaifeng <1131368173@qq.com>
2022-09-02 10:54:51 +0800

    添加nacos配置

---
+++ CoC-getway/src/main/java/com/coc/getway/GetWayApplication.java
@@ -0,0 +1,13 @@
+package com.coc.getway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GetWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GetWayApplication.class, args);
+    }
+}
